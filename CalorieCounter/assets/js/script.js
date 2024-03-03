95dbb13543f2d84966faf9d64addc0ef73fb835e
progchris00 <prog.christian07@gmail.com>
2024-03-03 19:51:07 +0800

    Create function to get calories from input

--- CalorieCounter/assets/js/script.js
+++ CalorieCounter/assets/js/script.js
@@ -1,5 +1,6 @@
 const addEntryButton = document.getElementById("add-entry");
 const entryDropdown = document.getElementById("entry-dropdown");
+let isError = false;
 
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
@@ -21,5 +22,23 @@ function addEntry() {
     <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" placeholder="Calories">`;
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+
+        if (invalidInputMatch) {
+            alert(`Invalid input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
+    }
+    return calories;
+}
+
 
 addEntryButton.addEventListener("click", addEntry);
