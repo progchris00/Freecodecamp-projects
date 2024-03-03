8f9b065615ee65c783e31bbd78d5e7fe10c85c45
progchris00 <prog.christian07@gmail.com>
2024-03-03 17:29:21 +0800

    Create a function to check wheather a character is valid or not

--- CalorieCounter/assets/js/script.js
+++ CalorieCounter/assets/js/script.js
@@ -2,3 +2,8 @@ function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, '');
 }
+
+function isValidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
