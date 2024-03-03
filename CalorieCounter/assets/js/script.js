ac11eac92c28b384696461c1fe27b5e2319f590f
progchris00 <prog.christian07@gmail.com>
2024-03-03 19:35:58 +0800

    Make variable name more descriptive

--- CalorieCounter/assets/js/script.js
+++ CalorieCounter/assets/js/script.js
@@ -12,15 +12,14 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-    const currentInput = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const inputCounts = document.querySelectorAll(`#${entryDropdown.value} input[type="number"]`).length + 1;
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = document.querySelectorAll(`#${entryDropdown.value} input[type="number"]`).length + 1;
     const HTMLString = `
-    <label for="${entryDropdown.value}-${inputCounts}-name">Entry ${inputCounts} Name</label>
-    <input id="${entryDropdown.value}-1-name" type="text" placeholder="Name">
-    <label for="${entryDropdown.value}-1-calories">Entry 1 Calories</label>
-    <input id="${entryDropdown.value}-1-calories" type="number" placeholder="Name">`;
-    currentInput.innerHTML += HTMLString;
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input id="${entryDropdown.value}-${entryNumber}-name" type="text" placeholder="Name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" placeholder="Calories">`;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
 addEntryButton.addEventListener("click", addEntry);
-
