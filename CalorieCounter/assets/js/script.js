41d5b373495932d7b1377c30da39e70ea20993a6
progchris00 <prog.christian07@gmail.com>
2024-03-07 12:02:41 +0800

    Nest container and add style to improve ux

--- CalorieCounter/assets/js/script.js
+++ CalorieCounter/assets/js/script.js
@@ -22,10 +22,16 @@ function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = document.querySelectorAll(`#${entryDropdown.value} input[type="number"]`).length + 1;
     const HTMLString = `
-    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input id="${entryDropdown.value}-${entryNumber}-name" type="text" placeholder="Name">
-    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-    <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" placeholder="Calories" min="0">`;
+    <div class="label-input-container">
+        <div class="label-input">
+            <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+            <input id="${entryDropdown.value}-${entryNumber}-name" type="text" placeholder="Name">
+        </div>
+            <div class="label-input">
+            <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+            <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" placeholder="Calories" min="0">
+        </div>
+    </div>`;
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
