b42d5bf25a81f3e539d4d2b9de53282a506a5b81
progchris00 <prog.christian07@gmail.com>
2024-03-12 10:28:22 +0800

    Enable edit and delete button

--- CalorieCounter/assets/js/script.js
+++ CalorieCounter/assets/js/script.js
@@ -43,7 +43,7 @@ function addEntry() {
         </span>
     </div>`;
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
-    editEntry();
+    enableEditDeleteButton();
 }
 
 function getCaloriesFromInputs(list) {
